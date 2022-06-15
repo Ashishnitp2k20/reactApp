56bd1e39e4c1ba14735c3f36872ca2b3f1bf7d81
Ashish Kumar <ashishrock057@gmail.com>
2022-06-15 19:01:19 +0530

    Updates

---
+++ precache-manifest.30d2d6261fc4612e2016f767761f78dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf3de919508c11d4b360",
+    "url": "/reactApp/static/css/main.7e5c80e0.chunk.css"
+  },
+  {
+    "revision": "bf3de919508c11d4b360",
+    "url": "/reactApp/static/js/main.bf3de919.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/reactApp/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/reactApp/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "e209b92f72c40f164f99",
+    "url": "/reactApp/static/js/runtime~main.e209b92f.js"
+  },
+  {
+    "revision": "224d48ca83c69db53ff92c4f23397e7d",
+    "url": "/reactApp/index.html"
+  }
+];
